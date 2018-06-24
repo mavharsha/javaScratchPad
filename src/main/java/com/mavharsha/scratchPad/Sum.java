7c3b0d4ad6d5c75064eb0132193690a2ea72ba5e
mavharsha <mavharsha@mavharsha.com>
2018-06-24 01:46:15 -0700

    Added bounded types

--- src/main/java/com/mavharsha/scratchPad/Sum.java
+++ src/main/java/com/mavharsha/scratchPad/Sum.java
@@ -1,5 +1,5 @@
 package com.mavharsha.scratchPad;
 @FunctionalInterface
-public interface Sum<T, K, V> {
+public interface Sum<T extends Number, K extends Number, V extends Number> {
     T sum(K first, V second);
 }
