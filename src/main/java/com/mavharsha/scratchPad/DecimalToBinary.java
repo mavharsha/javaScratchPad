08532fcd76fa8ef13d83c9d20d792323692dd6c1
sreema <sree.Mamilla@vsp.com>
2018-09-19 09:52:11 -0700

    Added Recursion

--- src/main/java/com/mavharsha/scratchPad/DecimalToBinary.java
+++ src/main/java/com/mavharsha/scratchPad/DecimalToBinary.java
@@ -18,6 +18,25 @@ public class DecimalToBinary {
         return BINARY_PREFIX + binary;
     }
 
+    public static String covertRecursively(int number) {
+        validate(number);
+        return BINARY_PREFIX + helper(number);
+    }
+
+    private static String helper(int number) {
+        if(number == 0) {
+            return "0";
+        }
+        if(number == 1) {
+            return "1";
+        }
+        String binary = "";
+        int remainder = number % 2;
+        binary = remainder + binary;
+        number = number / 2;
+        return helper(number) + binary;
+    }
+
     private static void validate(int number) {
         if(number < 0) {
             throw new IllegalArgumentException("Number cannot be less than 10");
--- src/test/java/com/mavharsha/scratchPad/DecimalToBinaryTest.java
+++ src/test/java/com/mavharsha/scratchPad/DecimalToBinaryTest.java
@@ -11,11 +11,24 @@ public class DecimalToBinaryTest {
         assertTrue(DecimalToBinary.convert(0).equalsIgnoreCase("0b0"));
         assertTrue(DecimalToBinary.convert(2).equalsIgnoreCase("0b10"));
         assertTrue(DecimalToBinary.convert(3).equalsIgnoreCase("0b11"));
+        // always remember
+        assertTrue(DecimalToBinary.convert(98).equalsIgnoreCase("0b1100010"));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void expectAnExceptionWhenNumberIsLessThanZero() {
         DecimalToBinary.convert(-1);
         DecimalToBinary.convert(-100);
+        DecimalToBinary.covertRecursively(-1);
+        DecimalToBinary.covertRecursively(-100);
+    }
+
+    @Test
+    public void testShouldReturnBinaryRecursively() {
+        assertTrue(DecimalToBinary.covertRecursively(0).equalsIgnoreCase("0b0"));
+        assertTrue(DecimalToBinary.covertRecursively(2).equalsIgnoreCase("0b10"));
+        assertTrue(DecimalToBinary.covertRecursively(3).equalsIgnoreCase("0b11"));
+        // always remember
+        assertTrue(DecimalToBinary.covertRecursively(98).equalsIgnoreCase("0b1100010"));
     }
 }
