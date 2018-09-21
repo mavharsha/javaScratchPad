d5bd1b1f30d64e9b7144e984678f5b48be8cd9e1
sree <mavharsha@mavharsha.com>
2018-09-20 23:43:26 -0700

    Added recursive Palindrome.

---
+++ src/main/java/com/mavharsha/scratchPad/Palindrome.java
@@ -0,0 +1,20 @@
+package com.mavharsha.scratchPad;
+
+public class Palindrome {
+
+    public boolean isPalindrome(String input){
+        return helper(input, 0, input.length() -1);
+    }
+
+    private boolean helper(String input, int beginIndex, int endIndex) {
+
+        if(beginIndex >= endIndex) {
+            return true;
+        }
+
+        if(input.charAt(beginIndex) == input.charAt(endIndex)) {
+            return helper(input, beginIndex + 1, endIndex -1);
+        }
+        return false;
+    }
+}
---
+++ src/test/java/com/mavharsha/scratchPad/PalindromeTest.java
@@ -0,0 +1,36 @@
+package com.mavharsha.scratchPad;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PalindromeTest {
+
+    private Palindrome palindrome;
+    @Before
+    public void setUp() throws Exception {
+        palindrome = new Palindrome();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        palindrome = null;
+    }
+
+    @Test
+    public void testIsPalindromWithSingleChar() {
+        assertTrue(palindrome.isPalindrome("A"));
+        assertTrue(palindrome.isPalindrome("B"));
+    }
+
+    @Test
+    public void testIsPalindromeWithMoreThanOneCharInInputString() {
+        assertTrue(palindrome.isPalindrome("aa"));
+        assertTrue(palindrome.isPalindrome("rotor"));
+
+        assertFalse(palindrome.isPalindrome("ab"));
+        assertFalse(palindrome.isPalindrome("motor"));
+    }
+}
