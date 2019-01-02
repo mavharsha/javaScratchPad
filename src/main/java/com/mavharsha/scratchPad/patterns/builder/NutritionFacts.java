a0133015f76c66a2a692cf4afd1dbe037036b6ad
mavharsha <sreeharsha.mamilla@gmail.com>
2019-01-01 23:12:19 -0800

    mend

--- src/main/java/com/mavharsha/scratchPad/patterns/builder/NutritionFacts.java
+++ src/main/java/com/mavharsha/scratchPad/patterns/builder/NutritionFacts.java
@@ -1,4 +1,64 @@
 package com.mavharsha.scratchPad.patterns.builder;
 
 public class NutritionFacts {
+
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+
+    public static class Builder {
+        // Required parameters
+        private final int servingSize;
+        private final int servings;
+
+        // Optional parameters
+        private  int calories = 0;
+        private  int fat = 0;
+        private  int sodium = 0;
+        private  int carbohydrate = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int value){
+            calories = value;
+            return this;
+        }
+
+        public Builder fat(int value){
+            fat = value;
+            return this;
+        }
+
+        public Builder sodium(int value){
+            sodium = value;
+            return this;
+        }
+
+        public Builder carbohydrate(int value){
+            carbohydrate = value;
+            return this;
+        }
+
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+
+    private NutritionFacts(Builder builder) {
+        this.servingSize = builder.servingSize;
+        this.servings = builder.servings;
+        this.calories = builder.calories;
+        this.fat = builder.fat;
+        this.sodium = builder.sodium;
+        this.carbohydrate = builder.carbohydrate;
+    }
 }
--- src/test/java/com/mavharsha/scratchPad/patterns/builder/NutritionFactsTest.java
+++ src/test/java/com/mavharsha/scratchPad/patterns/builder/NutritionFactsTest.java
@@ -1,5 +1,35 @@
+package com.mavharsha.scratchPad.patterns.builder;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class NutritionFactsTest {
 
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testShouldValidateObjectCreationWithDefaultParameters() {
+        NutritionFacts nutritionFacts = new NutritionFacts.Builder(10, 10).build();
+        assertNotNull(nutritionFacts);
+    }
+
+
+    @Test
+    public void testShouldValidateObjectCreationWithOptionalParameters() {
+        NutritionFacts nutritionFacts = new NutritionFacts.Builder(10, 10)
+                .calories(24)
+                .fat(22).carbohydrate(10).build();
+        assertNotNull(nutritionFacts);
+    }
 }
